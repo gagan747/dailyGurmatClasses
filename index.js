31b20f5cfbb68635dac27d7342fde0da4e03cf32
gurjot74 <gagan.singh@tothenew.com>
2022-12-23 19:01:47 +0530

    changes in hello7

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.use('/meow',(req,res)=>{
-          res.status(200)
+          res.status(204)
           console.log('hello')
           res.send('hitted')
           
